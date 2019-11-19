c96a271aaec41ba757b84461dc1637f31f3ea477
Ben Lardie <benlardie@gmail.com>
2019-11-19 17:24:50 -0500

    ball follows mouse and started on scroll

--- main.js
+++ main.js
@@ -54,6 +54,60 @@ const onMouseMoveHandler = (event) => {
         update(event)
     }
 }
+
+const $ = document.querySelector.bind(document)
+const $on = document.addEventListener.bind(document)
+
+let xmouse, ymouse
+$on('mousemove', (e) => {
+    xmouse = e.clientX || e.pageX
+    ymouse = e.clientY || e.pageY
+})
+
+const ball = $('#ball')
+let x = void 0,
+    y = void 0,
+    dx = void 0,
+    dy = void 0,
+    tx = 0,
+    ty = 0,
+    key = -1
+
+const followMouse = () => {
+    key = requestAnimationFrame(followMouse)
+
+    if (!x || !y) {
+        x = xmouse
+        y = ymouse
+    } else {
+        dx = (xmouse - x) * 0.125
+        dy = (ymouse - y) * 0.125
+        if(Math.abs(dx) + Math.abs(dy) < 0.1) {
+            x = xmouse
+            y = ymouse
+        } else {
+            x  += dx
+            y += dy
+        }
+    }
+    ball.style.left = x + 'px'
+    ball.style.top = y + 'px'
+}
+
+const text = document.querySelector('#scroll')
+
+document.addEventListener('scroll', () => {
+    const yoff = window.pageYOffset
+    const background_scale = 500
+
+    if (yoff < background_scale) {
+        const perc = yoff / background_scale
+        text.style.transform = `scale(${1 + 0.4 * perc})`
+    } 
+})
+
+
+followMouse()
 mouse.setOrigin(container)
 container.onmouseenter = onMouseEnterHandler
 // container.onmouseleave = onMouseLeaveHandler
