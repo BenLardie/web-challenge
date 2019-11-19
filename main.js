638f0ea671ae45fcc9d2d43357e662d3e6c8ecba
Ben Lardie <benlardie@gmail.com>
2019-11-19 15:51:20 -0500

    moving logo is working

---
+++ main.js
@@ -0,0 +1,63 @@
+const container = document.getElementById('container')
+const inner = document.getElementById('inner')
+
+let mouse = {
+    _x: 0,
+    _y: 0,
+    x: 0,
+    y: 0,
+    updatePosition: (event) => {
+        let e = event || window.event
+        this.x = e.clientX - this._x
+        this.y = (e.clientY - this._y) * -1
+    },
+    setOrigin: (e) => {
+        this._x = e.offsetLeft + Math.floor(e.offsetWidth / 2);
+        this._y = e.offsetTop + Math.floor(e.offsetHeight / 2);
+    }
+}
+
+const update =  (event) => {
+    mouse.updatePosition(event);
+    updateTransformStyle(
+        (event.y / inner.offsetHeight / 2).toFixed(2),
+        (event.x / inner.offsetWidth / 2).toFixed(2)
+    );
+};
+
+const updateTransformStyle = (x, y) => {
+    let style = "rotateX(" + x + "deg) rotateY(" + y + "deg)";
+    inner.style.transform = style;
+    inner.style.webkitTransform = style;
+    inner.style.mozTransform = style;
+    inner.style.msTransform = style;
+    inner.style.oTransform = style;
+};
+
+let counter = 0
+let updateRate = 5
+
+const isTimeToUpdate = () => {
+    return counter++ % updateRate === 0
+}
+
+const onMouseEnterHandler = (event) => {
+    update(event)
+}
+
+const onMouseLeaveHandler = () => {
+    inner.style = ''
+}
+
+const onMouseMoveHandler = (event) => {
+    if (isTimeToUpdate()) {
+        update(event)
+    }
+}
+mouse.setOrigin(container)
+container.onmouseenter = onMouseEnterHandler
+// container.onmouseleave = onMouseLeaveHandler
+container.onmousemove = onMouseMoveHandler
+
+
+
