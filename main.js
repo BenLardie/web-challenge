7ea2216c41119d4c1f60003ba2278851cca3fbc3
Ben Lardie <benlardie@gmail.com>
2019-11-19 22:51:16 -0500

    scroll progress coming

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const container = document.getElementById('container')
 const inner = document.getElementById('inner')
 
+window.addEventListener('load', () => {
+    const loader = document.querySelector('.loader')
+    loader.className += ' hidden'
+})
+
 let mouse = {
     _x: 0,
     _y: 0,
@@ -55,59 +60,66 @@ const onMouseMoveHandler = (event) => {
     }
 }
 
-const $ = document.querySelector.bind(document)
-const $on = document.addEventListener.bind(document)
+// const select = document.querySelector.bind(document)
+// const listener = document.addEventListener.bind(document)
 
-let xmouse, ymouse
-$on('mousemove', (e) => {
-    xmouse = e.clientX || e.pageX
-    ymouse = e.clientY || e.pageY
-})
+// let xmouse, ymouse
+// listener('mousemove', (e) => {
+//     xmouse = e.clientX || e.pageX
+//     ymouse = e.clientY || e.pageY
+// })
 
-const ball = $('#ball')
-let x = void 0,
-    y = void 0,
-    dx = void 0,
-    dy = void 0,
-    tx = 0,
-    ty = 0,
-    key = -1
+// const ball = select('#ball')
+// let x = void 0,
+//     y = void 0,
+//     dx = void 0,
+//     dy = void 0,
+//     tx = 0,
+//     ty = 0,
+//     key = -1
 
-const followMouse = () => {
-    key = requestAnimationFrame(followMouse)
+// const followMouse = () => {
+//     key = requestAnimationFrame(followMouse)
 
-    if (!x || !y) {
-        x = xmouse
-        y = ymouse
-    } else {
-        dx = (xmouse - x) * 0.125
-        dy = (ymouse - y) * 0.125
-        if(Math.abs(dx) + Math.abs(dy) < 0.1) {
-            x = xmouse
-            y = ymouse
-        } else {
-            x  += dx
-            y += dy
-        }
-    }
-    ball.style.left = x + 'px'
-    ball.style.top = y + 'px'
-}
+//     if (!x || !y) {
+//         x = xmouse
+//         y = ymouse
+//     } else {
+//         dx = (xmouse - x) * 0.125
+//         dy = (ymouse - y) * 0.125
+//         if(Math.abs(dx) + Math.abs(dy) < 0.1) {
+//             x = xmouse
+//             y = ymouse
+//         } else {
+//             x  += dx
+//             y += dy
+//         }
+//     }
+//     ball.style.left = x + 'px'
+//     ball.style.top = y + 'px'
+// }
 
-const text = document.querySelector('#scroll')
+const cursor = document.querySelector('#ball')
 
-document.addEventListener('scroll', () => {
-    const yoff = window.pageYOffset
-    const background_scale = 500
-
-    if (yoff < background_scale) {
-        const perc = yoff / background_scale
-        text.style.transform = `scale(${1 + 0.4 * perc})`
-    } 
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute('style', 'top: ' + (e.pageY + 10 ) + 'px; left: '+ (e.pageX + 10) + 'px;' )
 })
 
 
-followMouse()
+
+// window.addEventListener('scroll', () => {
+//     target = document.querySelectorAll('.scroll')
+//     console.log(target)
+//     target.forEach((element, index) => {
+//         console.log(element)
+//         let pos = window.pageYOffset * element[index].dataset.rate
+//         element[index].style.transform = 'translate3d(0px, '+ pos +'px, 0px)'
+//     });
+// })
+
+
+
+// followMouse()
 mouse.setOrigin(container)
 container.onmouseenter = onMouseEnterHandler
 // container.onmouseleave = onMouseLeaveHandler
