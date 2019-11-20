9f40bd19da1c52c3ceb786f82b6b92c75f9d4719
Ben Lardie <benlardie@gmail.com>
2019-11-19 22:53:30 -0500

    cleaned out some commented code

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ let mouse = {
     }
 }
 
-const update =  (event) => {
+const update = (event) => {
     mouse.updatePosition(event);
     updateTransformStyle(
         (event.y / inner.offsetHeight / 2).toFixed(2),
@@ -54,72 +54,20 @@ const onMouseLeaveHandler = () => {
     inner.style = ''
 }
 
-const onMouseMoveHandler = (event) => {
+const onMouseMoveHandler = event => {
     if (isTimeToUpdate()) {
         update(event)
     }
 }
 
-// const select = document.querySelector.bind(document)
-// const listener = document.addEventListener.bind(document)
-
-// let xmouse, ymouse
-// listener('mousemove', (e) => {
-//     xmouse = e.clientX || e.pageX
-//     ymouse = e.clientY || e.pageY
-// })
-
-// const ball = select('#ball')
-// let x = void 0,
-//     y = void 0,
-//     dx = void 0,
-//     dy = void 0,
-//     tx = 0,
-//     ty = 0,
-//     key = -1
-
-// const followMouse = () => {
-//     key = requestAnimationFrame(followMouse)
-
-//     if (!x || !y) {
-//         x = xmouse
-//         y = ymouse
-//     } else {
-//         dx = (xmouse - x) * 0.125
-//         dy = (ymouse - y) * 0.125
-//         if(Math.abs(dx) + Math.abs(dy) < 0.1) {
-//             x = xmouse
-//             y = ymouse
-//         } else {
-//             x  += dx
-//             y += dy
-//         }
-//     }
-//     ball.style.left = x + 'px'
-//     ball.style.top = y + 'px'
-// }
 
 const cursor = document.querySelector('#ball')
 
 document.addEventListener('mousemove', e => {
-    cursor.setAttribute('style', 'top: ' + (e.pageY + 10 ) + 'px; left: '+ (e.pageX + 10) + 'px;' )
+    cursor.setAttribute('style', 'top: ' + (e.pageY + 10) + 'px; left: ' + (e.pageX + 10) + 'px;')
 })
 
 
-
-// window.addEventListener('scroll', () => {
-//     target = document.querySelectorAll('.scroll')
-//     console.log(target)
-//     target.forEach((element, index) => {
-//         console.log(element)
-//         let pos = window.pageYOffset * element[index].dataset.rate
-//         element[index].style.transform = 'translate3d(0px, '+ pos +'px, 0px)'
-//     });
-// })
-
-
-
-// followMouse()
 mouse.setOrigin(container)
 container.onmouseenter = onMouseEnterHandler
 // container.onmouseleave = onMouseLeaveHandler
